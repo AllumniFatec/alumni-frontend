6cb6a1bbdf96b9f375c6c95a158e99fca54d0543
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-10-12 20:27:49 -0300

    .env config

---
+++ src/apis/mockPostsApi.tsx
@@ -0,0 +1,15 @@
+export class MockPostsApi {
+  static async getUser(userId: string) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async updateUser(userId: string, data: any) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}

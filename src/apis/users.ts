fa7c6fd221dcf003c2f60b1393a8c95b3d4cd2ec
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-10-19 15:51:01 -0300

    user get token

---
+++ src/apis/users.ts
@@ -0,0 +1,22 @@
+import { User } from "@/models/users";
+import { AuthStorage } from "@/store/auth";
+import axios from "axios";
+const API_BASE_URL = "http://localhost:3001";
+
+export class UserApi {
+  static async getUsers(): Promise<User[]> {
+    try {
+      const token = AuthStorage.getToken();
+      const response = await axios.get(`${API_BASE_URL}/list-users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.info(`users loaded: ${response.data.length}`);
+      return response.data;
+    } catch (error) {
+      console.error("getUsers error", error);
+      throw error;
+    }
+  }
+}

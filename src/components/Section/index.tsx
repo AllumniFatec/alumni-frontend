1c4f4e9dd753dc7ae27854473a248ac4f18e65e1
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2026-03-15 15:33:58 -0300

    fix navBar responsiviness and section

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -8,17 +8,15 @@ interface SectionProps {
 
 export const Section = ({ title, children, className = "" }: SectionProps) => {
   return (
-    <section className={cn(`mb-12`, className)}>
-      <div className="mb-8">
-        <h2 className="text-3xl font-bold text-info text-left mb-4">{title}</h2>
+    <section className={cn(`mb-10`, className)}>
+      <div className="mb-6">
+        <h2 className="text-xl font-bold text-info text-left mb-2">{title}</h2>
         <div className="flex items-center">
-          <div className="h-4 bg-red-600 w-40 rounded-full"></div>
-          <div className="h-1 bg-red-600 flex-1"></div>
+          <div className="h-1 bg-primary w-8 rounded-full"></div>
+          <div className="h-px bg-primary flex-1"></div>
         </div>
       </div>
-      <div className="text-center text-gray-600 leading-relaxed">
-        {children}
-      </div>
+      <div className="text-gray-600 leading-relaxed">{children}</div>
     </section>
   );
 };
